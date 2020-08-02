3d619b149a6f6cd9b108549848da63b1a4e05dd0
KimHS0915 <remember2552@gmail.com>
2020-08-03 01:01:43 +0900

    Add Patent model

--- patents/models.py
+++ patents/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Patent(models.Model):
+    publication_number = models.CharField(max_length=20, primary_key=True)
+    application_number = models.CharField(max_length=20)
+    country_code = models.CharField(max_length=5)
+    filing_date = models.DateField(null=True)
+    assignee = models.CharField(max_length=500, null=True)
+    title = models.CharField(max_length=500, null=True)
+    abstract = models.TextField(null=True)
+    claims = models.TextField(null=True)
+
+    class Meta:
+        db_table = 'patents1000'
+
+    def __str__(self):
+        return self.publication_number
