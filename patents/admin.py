d70a3b2f36ceaffe4a7d68a1d44d74c3ddd9dc87
KimHS0915 <remember2552@gmail.com>
2020-08-04 23:32:52 +0900

    Add django admin

--- patents/admin.py
+++ patents/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Patent
+
+@admin.register(Patent)
+class PatentAdmin(admin.ModelAdmin):
+    list_display = [
+        'publication_number',
+        'title',
+        'assignee',
+        'filing_date',
+    ]
+    list_display_links = ['publication_number', 'title']
+    list_filter = ('country_code', 'filing_date')
+    search_fields = ['title', 'abstract', 'claims']
+    list_per_page = 20
