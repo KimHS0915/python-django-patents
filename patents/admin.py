c29d08ef8dce361cf089f115a12c887acfd1150d
KimHS0915 <remember2552@gmail.com>
2020-08-05 00:02:52 +0900

    Customize admin page
    Add django-admin-rangefilter
    Add action : export as csv

--- patents/admin.py
+++ patents/admin.py
@@ -1,7 +1,12 @@
+import csv
+
+from django.http import HttpResponse
 from django.contrib import admin
+from rangefilter.filter import DateRangeFilter
 
 from .models import Patent
 
+# Register your models here.
 @admin.register(Patent)
 class PatentAdmin(admin.ModelAdmin):
     list_display = [
@@ -11,6 +16,21 @@ class PatentAdmin(admin.ModelAdmin):
         'filing_date',
     ]
     list_display_links = ['publication_number', 'title']
-    list_filter = ('country_code', 'filing_date')
+    list_filter = ('country_code', ('filing_date', DateRangeFilter),)
     search_fields = ['title', 'abstract', 'claims']
     list_per_page = 20
+    actions = ['export_as_csv']
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
