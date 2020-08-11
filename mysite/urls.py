188594bd78795573b344b9963bc0cc345985f524
KimHS0915 <remember2552@gmail.com>
2020-08-11 18:39:26 +0900

    Add home

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import HomeView
+
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
     path('patents/', include('patents.urls')),
 ]
