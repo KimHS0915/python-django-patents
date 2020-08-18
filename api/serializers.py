d7eb99dc27435dc15ae0ddf160263583feb4eb8a
KimHS0915 <remember2552@gmail.com>
2020-08-18 20:41:14 +0900

    Add PatentDetailSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,18 @@ class PatentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patent
         fields = ['publication_number', 'title', 'abstract', 'claims']
-		
+
+
+class PatentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patent
+        fields = [
+            'publication_number',
+            'application_number',
+            'country_code',
+            'filing_date',
+            'assignee',
+            'title',
+            'abstract',
+            'claims',
+        ]
