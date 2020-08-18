2df466e0f7b887c65039a2b751b485ac9d65468d
KimHS0915 <remember2552@gmail.com>
2020-08-18 20:55:31 +0900

    Add APIView

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
+  
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import PatentListViewset
+from .views import PatentListViewset, PatentListAPIView, PatentDetailAPIView, PatentCreateAPIView, PatentUpdateAPIView, PatentDeleteAPIView
 
 
 router = routers.DefaultRouter()
@@ -9,4 +10,9 @@ router.register('patents', PatentListViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('list/', PatentListAPIView.as_view(), name='api_list'),
+    path('list/<str:pk>/', PatentDetailAPIView.as_view(), name='api_detail'),
+    path('new/', PatentCreateAPIView.as_view(), name='api_new'),
+    path('list/<str:pk>/edit/', PatentUpdateAPIView.as_view(), name='api_edit'),
+    path('list/<str:pk>/delete/', PatentDeleteAPIView.as_view(), name='api_delete')
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,40 @@
-from rest_framework import viewsets, permissions
+from rest_framework import generics, viewsets, permissions
 
 from patents.models import Patent
-from .serializers import PatentSerializer
+from .serializers import PatentSerializer, PatentDetailSerializer
 
 
 class PatentListViewset(viewsets.ModelViewSet):
     queryset = Patent.objects.all().order_by('publication_number')
     serializer_class = PatentSerializer
     permission_classes = [permissions.IsAuthenticated]
-	
+
+
+class PatentListAPIView(generics.ListAPIView):
+    queryset = Patent.objects.all()
+    serializer_class = PatentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PatentDetailAPIView(generics.RetrieveAPIView):
+    queryset = Patent.objects.all()
+    serializer_class = PatentDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PatentCreateAPIView(generics.CreateAPIView):
+    queryset = Patent.objects.all()
+    serializer_class = PatentDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PatentUpdateAPIView(generics.UpdateAPIView):
+    queryset = Patent.objects.all()
+    serializer_class = PatentDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PatentDeleteAPIView(generics.DestroyAPIView):
+    queryset = Patent.objects.all()
+    serializer_class = PatentSerializer
+    permission_classes = [permissions.IsAuthenticated]
